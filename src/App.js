b53b3ea0c8cc24897970880e9fd6438585ace4cc
Christine Kim <cledere87@gmail.com>
2022-09-02 08:49:32 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import "./styles.css";
+import React, { useState } from "react";
 
 export default function App() {
-  return (
+    
+    const [inputText, setInputText] = useState("");
+    const [todoItems, setTodoItems] = useState([]);
+    
+    function handleChange(event){
+        const newValue= event.target.value;
+        setInputText(newValue);
+    }
+    function addTodo() {
+        setTodoItems(prevItems => {
+            return [...prevItems, inputText];
+        });
+        setInputText("");
+     }
+
+  return (      
     <div className="App">
+      <div className="container">
         <div className="heading">
             <h1>To-Do List</h1>
         </div>
-      <h2>Start editing to see some magic happen!</h2>
+        <div className="form">
+            <form>
+            <input onChange={handleChange} type="text" value={inputText} />
+            <button onClick={addTodo}><span>Add</span></button>
+            </form>
+        </div>
+        <div>
+            <ul>
+                {todoItems.map(todoItem => {
+                    <li>{todoItem}</li>
+                })}
+            </ul>
+        </div>
+      </div>
     </div>
   );
 }
